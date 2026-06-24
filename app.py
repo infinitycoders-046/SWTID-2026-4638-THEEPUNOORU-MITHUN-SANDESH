3cf44d2c15f06fa200edb29be5cc978d8c5bb873
infinitycoders-046 <infinitycoders@zohomail.in>
2026-06-24 15:07:31 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/dashboard01')
+def dashboard01():
+    return render_template('dashboard01.html')
+
+@app.route('/dashboard02')
+def dashboard02():
+    return render_template('dashboard02.html')
+
+@app.route('/story')
+def story():
+    return render_template('story.html')
+
+if __name__ == '__main__':
+    # debug=True automatically restarts the server when you make changes
+    app.run(debug=True)
